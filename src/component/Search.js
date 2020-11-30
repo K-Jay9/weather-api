7094309356290afcbd1a77db7f44b96a43ec6b08
kjempire9 <jcreigns9@gmail.com>
2020-11-30 21:57:15 +0300

    completed the home page card

---
+++ src/component/Search.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Search() {
+    return (
+        <div className='container'>
+            <div className="card border-primary mb-3 search">
+                <div className="card-body text-primary">
+                    <h5 className="card-title text-center"> Weather App</h5>
+
+                    <div className='search-button'>
+                        <form className="form-inline">
+                            <input className="form-control" type="search" placeholder="Enter Location" aria-label="Search"></input>
+
+                            <button className="btn btn-primary " type="submit">
+                            <i className="fa fa-search"></i></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Search

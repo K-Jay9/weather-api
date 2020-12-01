8a491d34bc9429b9eb687c3e3a53305931275d35
kjempire9 <jcreigns9@gmail.com>
2020-12-01 15:15:40 +0300

    complete card with timestamps

--- src/component/Result.js
+++ src/component/Result.js
@@ -23,7 +23,8 @@ class Result extends Component {
                     console.log(result);
                     this.setState({
                         isLoaded : true,
-                        items : result.current
+                        items : result.current,
+                        locate : result.location
                     })
                 },
                 (error) => {
@@ -35,46 +36,56 @@ class Result extends Component {
             )
     }
     render() {
-        const { error, isLoaded, items } = this.state;
+        const { error, isLoaded, items , locate} = this.state;
         if (error) {
           return <div>Error: {error.message}</div>;
         } else if (!isLoaded) {
-          return <div>Loading...</div>;
+          return <div>
+              <span>Loading ...</span>
+          </div>;
         } else {
         return (
             <div className="container">
 
-            <div className="card">
-                <div className="card-body">
-                    <h5 className="card-title align-center">Current Weather</h5>
+                <div className="card result">
+                    <div className="card-body grey">
+                        <h5 className="card-title align-center">Current Weather</h5>
+                        <img src={items.weather_icons[0]} alt='weather icon' className='rounded'></img>
+                    </div>
+                        <span className='text-muted right'>
+                            Observation Time : {items.observation_time}</span>
+                        <span className='text-muted right'>
+                            Location: {locate.name}, {locate.country} </span>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">
+                            {items.weather_descriptions[0]}
+                        </li>
+                        <li className="list-group-item">
+                            Temperature : {items.temperature} &#8451;
+                        </li>
+                        <li className="list-group-item">
+                            Cloudcover : {items.cloudcover} oktas
+                        </li>
+                        <li className="list-group-item">
+                            Humidity : {items.humidity} g.m<sup>-3</sup>
+                        </li>
+                        <li className="list-group-item">
+                            Precipitation : {items.precip} mm
+                        </li>
+                        <li className="list-group-item">
+                            Visibility : {items.visibility} M
+                        </li>
+                        <li className="list-group-item">
+                            Wind Degree : {items.wind_degree} &#9702;
+                        </li>
+                        <li className="list-group-item">
+                            Wind Direction : {items.wind_dir}
+                        </li>
+                        <li className="list-group-item">
+                            Wind Speed : {items.wind_speed} Km/h
+                        </li>
+                    </ul>
                 </div>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item">
-                        Temperature : {items.temperature} &#8451;
-                    </li>
-                    <li className="list-group-item">
-                        Cloudcover : {items.cloudcover} oktas
-                    </li>
-                    <li className="list-group-item">
-                        Humidity : {items.humidity} g.m<sup>-3</sup>
-                    </li>
-                    <li className="list-group-item">
-                        Precipitation : {items.precip} mm
-                    </li>
-                    <li className="list-group-item">
-                        Visibility : {items.visibility} M
-                    </li>
-                    <li className="list-group-item">
-                        Wind Degree : {items.wind_degree} &#9702;
-                    </li>
-                    <li className="list-group-item">
-                        Wind Direction : {items.wind_dir}
-                    </li>
-                    <li className="list-group-item">
-                        Wind Speed : {items.wind_speed} Km/h
-                    </li>
-                </ul>
-            </div>
             </div>
         )
         }
